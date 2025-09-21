15a763f5c92e78bc082dd9874011606ea4596291
Valentina 1 <valentina.risso@correo.ucu.edu.uy>
2025-09-21 19:36:40 -0300

    wizards

--- src/Library/SpellBook.cs
+++ src/Library/SpellBook.cs
@@ -4,11 +4,6 @@ public class SpellBook
 {
     public List<Spell> spells = new List<Spell>();
 
-    public SpellBook(Spell[] initialSpells) // Recibe muchos hechizos
-    {
-        this.spells = new List<Spell>(initialSpells); // Convertir a List
-    }
-
     public void AddSpell(Spell spell)
     {
         spells.Add(spell);
