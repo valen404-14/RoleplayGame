7cfda59857893bfc8c0f364cd6f0c360715ad680
Guada <guadalupe.piva@correo.ucu.edu.uy>
2025-09-21 18:45:57 -0300

    add name to spell

--- src/Library/Spell.cs
+++ src/Library/Spell.cs
@@ -2,12 +2,15 @@ namespace Program;
 
 public class Spell
 {
+    private string name;
     private int magic; // Por ahora solo tendrá magic, más adelante puede tener otros atributos o compartamientos
     
-    public Spell(int magic)
+    public Spell(string name, int magic)
     {
+        this.name = name; 
         this.magic = magic;
     }
 
     public int GetMagic() => magic;
+    public string GetName() => name;
 }
