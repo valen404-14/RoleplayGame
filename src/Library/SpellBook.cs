8fe3fb3663c113b548426668931e0c1ec4c22692
Guada <guadalupe.piva@correo.ucu.edu.uy>
2025-09-21 18:19:02 -0300

    spell and spell book

---
+++ src/Library/SpellBook.cs
@@ -0,0 +1,11 @@
+namespace Program;
+
+public class SpellBook
+{
+    public List<Spell> spells = new List<Spell>();
+
+    public SpellBook(Spell[] spells) // Recibe muchos hechizos
+    {
+        this.spells = this.spells;
+    }
+}
