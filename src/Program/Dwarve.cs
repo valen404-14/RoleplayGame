5a481159485d7039234e33eacfbea1647e932d47
Ian de Lima <ian.delima@correo.ucu.edu.uy>
2025-09-21 17:47:00 -0300

    Clase Dwarve e Item

---
+++ src/Program/Dwarve.cs
@@ -0,0 +1,43 @@
+namespace Program;
+
+public class Dwarves
+{
+    private int health = 200;
+    private int attack = 20;
+    private int defense = 50;
+    private List<Item> items = new List<Item>();
+
+    public void UseItem(Item item)
+    {
+        this.defense += item.GetDefense();
+        this.attack += item.GetAttack();
+        this.health += item.GetHealth();
+        items.Add(item);
+    }
+
+    public int GetTotalAttack() => attack;
+    public int GetTotalDefense() => defense;
+
+    public void ReciveAttack(int damage)
+    {
+        if (damage <= defense)
+        {
+            defense -= damage;
+        }
+        else
+        {
+            defense -= damage;
+            health += defense;
+            defense = 0;
+        }
+    }
+
+    public void Heal()
+    {
+        if (health < 200)
+        {
+            health = 200;
+        }
+    }
+
+}
