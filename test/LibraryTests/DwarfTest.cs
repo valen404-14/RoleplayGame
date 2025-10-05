01ad1d1e97c6d2995dd547a248f0bfca84a9dd87
Valentina 1 <valentina.risso@correo.ucu.edu.uy>
2025-10-05 14:27:24 -0300

    Nuevos tests para Dwarf, Elf y Wizard

--- test/LibraryTests/DwarfTest.cs
+++ test/LibraryTests/DwarfTest.cs
@@ -6,12 +6,16 @@ public class DwarfTest
 {
     private Dwarf _dwarf;
     private Item _sword;
+    private Item _helmet;
+    private Item _healthPotion;
     
     [SetUp]
     public void Setup()
     {
         _dwarf = new Dwarf();
         _sword = new Item("Espada", 100, 0, 0, 0,false);
+        _helmet = new Item("Casco", 0, 30, 0, 0, false);
+        _healthPotion=new Item("Poción de vida",0,0,50,0,false);
     }
     [Test]
     public void TestGetTotalAttack()
@@ -38,5 +42,26 @@ public class DwarfTest
         _dwarf.UseItem(magicWand); // No debería modificar el ataque, ya que es un elemento mágico y solo le corresponde a los magos
         
         Assert.That(_dwarf.GetTotalAttack(), Is.EqualTo(initialAttack));
+    }
+    [Test]
+    public void HealTest1()
+    {
+        _dwarf.UseItem(_healthPotion); //le suma 50 a health (en total 250)
+        _dwarf.UseItem(_helmet); //le suma 30 a defense (en total 80).
+        _dwarf.ReceiveAttack(15); //el damage al ser menor que defense, defense queda con 65 (50+30-15).
+        _dwarf.Heal();//Como health actual (250) es mayor que 200, al utilizar el método Heal, debería devolver el valor actual de health (250)
+        Assert.That(_dwarf.GetTotalHealth(), Is.EqualTo(250));
+        Assert.That(_dwarf.GetTotalDefense(), Is.EqualTo(65));
+    }
+    [Test]
+    public void HealTest2()
+    {
+        _dwarf.UseItem(_healthPotion); // le suma 50 a health (en total 250)
+        _dwarf.UseItem(_helmet); //le suma 30 a defense (en total 80).
+        _dwarf.ReceiveAttack(200);//Como defense es 80, y health es 250, al recibir un ataque de 200 se queda sin defensa, y con 130 de health
+        Assert.That(_dwarf.GetTotalHealth(), Is.EqualTo(130));
+        Assert.That(_dwarf.GetTotalDefense(), Is.EqualTo(0));
+        _dwarf.Heal();//al usar Heal, y tener menos de 200 de health (130), ahora health se reestablece en el valor inicial (200)
+        Assert.That(_dwarf.GetTotalHealth(), Is.EqualTo(200));
     }
 }
--- test/LibraryTests/WizardTest.cs
+++ test/LibraryTests/WizardTest.cs
@@ -9,6 +9,7 @@ public class WizardTest
     private Item _horrocrux;
     private Spell _wingardiumLeviosa;
     private Spell _avadaKedavra;
+    private Item _healthPotion;
     
     [SetUp]
     public void Setup()
@@ -18,6 +19,7 @@ public class WizardTest
         _horrocrux = new Item("Horrocrux", 0, 100, 0, 40,true);
         _wingardiumLeviosa = new Spell("Wingardium Leviosa", 20);
         _avadaKedavra = new Spell("Avada Kedavra", 100);
+        _healthPotion=new Item("Poción de vida",0,0,50,0,false);
     }
     
     [Test]
@@ -28,5 +30,34 @@ public class WizardTest
         _wizard.UseItem(_horrocrux);//+40
         _wizard.UseItem(_magicWand);//+50
         Assert.That(_wizard.GetTotalMagic(),Is.EqualTo(310));
+    }
+
+    [Test]
+    public void ReceiveAttackTest()
+    {
+        _wizard.ReceiveAttack(150);//health queda en 150: defense queda en 0.
+        Assert.That(_wizard.GetTotalHealth(), Is.EqualTo(150));
+    }
+
+    [Test]
+    public void HealTest1()
+    {
+        _wizard.UseItem(_healthPotion); //+50 a health (300)
+        _wizard.ReceiveAttack(45); //45<50, defense queda en 5. Health no se modifica (queda en 300).
+        Assert.That(_wizard.GetTotalHealth(), Is.EqualTo(300));
+        Assert.That(_wizard.GetTotalDefense(), Is.EqualTo(5));
+        _wizard.Heal(); //al tener más de 250 de health (200), al usar este método debería volver al valor de health actual (300)
+        Assert.That(_wizard.GetTotalHealth(), Is.EqualTo(300));
+    }
+    
+    [Test]
+    public void HealTest2()
+    {
+        _wizard.UseItem(_healthPotion); //+50 a health (300)
+        _wizard.ReceiveAttack(150); //50<150, defense queda en 0. Health queda en 200.
+        Assert.That(_wizard.GetTotalHealth(), Is.EqualTo(200));
+        Assert.That(_wizard.GetTotalDefense(), Is.EqualTo(0));
+        _wizard.Heal(); //al tener menos de 250 de health (200), al usar este método debería reestablecer health (devolviento del valor inicial, 250)
+        Assert.That(_wizard.GetTotalHealth(), Is.EqualTo(250));
     }
 }
